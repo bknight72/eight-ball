b6efeb64fa4b96ebcc5f5d53effa9cb817c13f96
bknight72 <benknighty@gmail.com>
2021-08-16 11:10:54 -0600

    making an 8 ball app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[200],
+      appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.blue[800],
+          centerTitle: true,
+          title: Text("Whisper me a question")),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Expanded(
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
